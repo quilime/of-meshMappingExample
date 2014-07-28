ac50cc4cda5218e8bd76c8efc6618f2d89eb545d
Gabriel Dunne <gdunne@quilime.com>
2014-07-28 15:24:56 -0700

    Fixing vert indicies

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 	ofSetVerticalSync(true);
-	mesh.load("landscape-bot.ply");
+	mesh.load("landscape.ply");
 }
 
 //--------------------------------------------------------------
