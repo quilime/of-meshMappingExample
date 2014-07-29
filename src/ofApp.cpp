255051f07f74300b0ac03ed7a6f0305db6e082bb
Gabriel Dunne <gdunne@quilime.com>
2014-07-29 15:28:31 -0700

    Cleaning up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,49 +3,42 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
   
-  isShaderDirty = true;
   showHelp = true;
   editMode = false;
   camMouse = false;
   mouseDragging = false;
   nearestIndex = 0;
+  isShaderDirty = true;
   
-  // load camera setting
+  // load camera settings
   ofxLoadCamera(cam, "cameraSettings");
+  // initialize scene with mouse input disabled
+  cam.disableMouseInput();
   
-  // set vertical sync to true
 	ofSetVerticalSync(true);
   
   // load mesh
 	mesh.load("landscape-squ.ply");
 //  mesh.load("mesh-tweaked.ply");
-  //mesh.smoothNormals( 15 );
-//  mesh.usingIndices();
+//  mesh.smoothNormals( 15 );
   
+  // reference sphere
   sphere.setRadius( 20 );
   
   // set up lights
-  ofSetGlobalAmbientColor(ofColor(0,0,0));
+  ofSetGlobalAmbientColor(ofColor(0, 0, 0));
   ofSetSmoothLighting(true);
   light.setPointLight();
   light.setDiffuseColor( ofFloatColor(0.6f, 0.6f, 0.6f));
   light.setSpecularColor( ofFloatColor(1.0f, 1.0f, 1.0f));
-  light.setAttenuation(0.5, 0, 0);
+//  light.setAttenuation(0.5, 0, 0);
 //  setAttenuation(float constant=1.f, float linear=0.f, float quadratic=0.f))
-  
   
   // set up material
   // shininess is a value between 0 - 128, 128 being the most shiny //
 	material.setShininess( 120 );
-  // the light highlight of the material //
-	material.setSpecularColor(ofColor(255, 255, 255, 255));
-  material.setAmbientColor(ofColor(0,0,0));
-  
-  ofSetSphereResolution(24);
-  
-  
-
-  
+	material.setSpecularColor(ofColor(255, 255, 255));
+  material.setAmbientColor(ofColor(0, 0, 0));
 }
 
 //--------------------------------------------------------------
@@ -53,7 +46,6 @@ void ofApp::update(){
   
   // move light around
   light.setPosition(cos(ofGetElapsedTimef()) * 100.0, 40, sin(ofGetElapsedTimef()) * 100.0);
-  
   
   // update shader
   if (isShaderDirty){
@@ -66,7 +58,6 @@ void ofApp::update(){
 		err = glGetError();	// we need this to clear out the error buffer.
 		ofLogNotice() << "Loaded Shader: " << err;
     
-		
 		isShaderDirty = false;
 	}
   
@@ -75,21 +66,28 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
   
-	glShadeModel(GL_SMOOTH);
-	glProvokingVertex(GL_LAST_VERTEX_CONVENTION);
+//	glShadeModel(GL_SMOOTH);
+//	glProvokingVertex(GL_LAST_VERTEX_CONVENTION);
 
-  ofEnableDepthTest();
+  
   
   if (editMode) {
-    // background gradient
+    // if editing, show background gradient
     ofBackgroundGradient(ofColor(64), ofColor(0));
   } else {
+    // black bg
     ofBackground(ofColor(0));
   }
   
   // set main color to white
   ofSetColor(255);
-
+  
+  // enable depth test so objects draw in correct z-order
+  ofEnableDepthTest();
+  //  glEnable(GL_DEPTH_TEST);
+  //	glEnable(GL_CULL_FACE);
+  //  glCullFace(GL_BACK);  
+  
   // begin camera
 	cam.begin();
   
@@ -100,26 +98,23 @@ void ofApp::draw(){
   //	light.setGlobalPosition(1000, 1000, 1000);
   //	light.lookAt(ofVec3f(0,0,0));
 
-  glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-  glCullFace(GL_BACK);
   
   // start material
   ofSetColor(light.getDiffuseColor());
   material.begin();
-//  ofFill();
-//  ofSetColor(255);
+  ofFill();
+  ofSetColor(255);
   
-	shader->begin();
-  shader->setUniform1f("shouldRenderNormals", 0.0);
-  shader->setUniform1f("shouldUseFlatShading", 0.0);
-  glShadeModel(GL_FLAT);
-  glProvokingVertex(GL_FIRST_VERTEX_CONVENTION);		// OpenGL default is GL_LAST_VERTEX_CONVENTION
+//	shader->begin();
+//  shader->setUniform1f("shouldRenderNormals", 0.0);
+//  shader->setUniform1f("shouldUseFlatShading", 0.0);
+//  glShadeModel(GL_FLAT);
+//  glProvokingVertex(GL_FIRST_VERTEX_CONVENTION);		// OpenGL default is GL_LAST_VERTEX_CONVENTION
 	// restores shade model
-	glPopAttrib();
+//	glPopAttrib();
 	// restores vertex convention defaults.
-	glProvokingVertex(GL_LAST_VERTEX_CONVENTION);
-  shader->end();
+//	glProvokingVertex(GL_LAST_VERTEX_CONVENTION);
+//  shader->end();
   
   
   // test sphere
@@ -129,50 +124,48 @@ void ofApp::draw(){
   
   // draw mesh faces
   mesh.draw();
+ 
   
-  // end material
-  material.end();
-  
-
   // draw light position
   if (editMode) {
+    ofSetColor(255);
     ofFill();
-    ofSetColor(light.getDiffuseColor());
     light.draw();
   }
   
   
+  // end material
+  material.end();
+
   // end lighting
-  
-	glDisable(GL_CULL_FACE);
-	glDisable(GL_DEPTH_TEST);
-  
-  ofDisableLighting();
   light.disable();
+  ofDisableLighting();
   
   
-  // draw verts
+  // draw verts when editing
   if (editMode) {
-    // set color to white
-    ofSetColor(ofColor::white);
     // set point size to 2
     glPointSize(2);
-    // draw verts
+    // draw all verts as white
+    ofSetColor(ofColor::white);
     mesh.drawVertices();
   }
   
   
   // end camera
   cam.end();
+  ofDisableDepthTest();
+  //	glDisable(GL_CULL_FACE);
+  //	glDisable(GL_DEPTH_TEST);
   
 
   // vert selection
   if (editMode) {
     
-    // create vec2 of the mouse
+    // create vec2 of the mouse for reference
     ofVec2f mouse(mouseX, mouseY);
     
-    // if not mousedragging, find the nearest vert
+    // if not dragging the mouse, find the nearest vert
     if (!mouseDragging) {
       // loop through all verticies in mesh and find the nearest vert position and index
       int n = mesh.getNumVertices();
@@ -188,47 +181,38 @@ void ofApp::draw(){
       }
     }
     
-    // set color to gray
+    // draw a gray line from the nearest vertex to the mouse position
     ofSetColor(ofColor::gray);
-    
-    // draw a line from the nearest vertex to the mouse position
     ofLine(nearestVertex, mouse);
     
     // draw a cirle around the nearest vertex
     ofNoFill();
-    ofSetColor(ofColor::yellow);
+    ofSetColor(ofColor::magenta);
     ofSetLineWidth(2);
     ofCircle(nearestVertex, 4);
     ofSetLineWidth(1);
     
-    // move vertex with mouse while dragging
+    // edit position of nearest vert with mousedrag
     if (!camMouse && mouseDragging) {
-      
-      ofVec3f stow = cam.screenToWorld( ofVec3f(mouse.x, mouse.y, nearestVertex.z) );
-      
-      mesh.setVertex(nearestIndex, stow);
-      
+      mesh.setVertex(nearestIndex, cam.screenToWorld(ofVec3f(mouse.x, mouse.y, nearestVertex.z)));
     }
     
-    // draw some text with then nearest vertex index
-    ofVec2f offset(10, -10);
-    ofDrawBitmapStringHighlight(ofToString(nearestIndex), mouse + offset);
+    // draw a label with the nearest vertex index
+    ofDrawBitmapStringHighlight(ofToString(nearestIndex), mouse + ofVec2f(10, -10));
   }
-  
-  
   
   // draw info text
   if (showHelp) {
     stringstream ss;
-    ss << "Framerate: " << ofToString(ofGetFrameRate(),0) << "\n";
-    ss << "f : Toggle Fullscreen" << "\n";
-    ss << "h : Toggle Help Text" << "\n";
-    ss << "c : Toggle Camera Control" << (camMouse == true ? " ON" : " OFF") << "\n";
-    ss << "TAB : Toggle Mesh Edit Mode" << (editMode == true ? " ON" : " OFF") << "\n";
-    ss << "s : Save Mesh and Camera" << "\n";
-    ss << "l : Load Mesh and Camera" << "\n";
+    ss << "Framerate " << ofToString(ofGetFrameRate(),0) << "\n";
+    ss << "  f : Toggle Fullscreen" << "\n";
+    ss << "  h : Toggle Help Text" << "\n";
+    ss << "  c : Toggle Camera Control : " << (camMouse == true ? "ON" : "OFF") << "\n";
+    ss << "TAB : Toggle Mesh Edit : " << (editMode == true ? "ON" : "OFF") << "\n";
+    ss << "  s : Save Scene" << "\n";
+    ss << "  l : Load Scene";
     ofSetColor(ofColor::white);
-    ofDrawBitmapString(ss.str().c_str(), 20, 20);
+    ofDrawBitmapStringHighlight(ss.str().c_str(), ofVec3f(10, 20), ofColor(45), ofColor(255));
   }
 }
 
@@ -237,22 +221,24 @@ void ofApp::keyPressed(int key){
   
 	switch(key) {
       
-    // toggle 'f' for fullscreen
+    // toggle fullscreen
 		case 'f':
 			ofToggleFullscreen();
 			break;
 
-    // toggle 'c' for camera control
+    // toggle camera control
     case 'c':
       camMouse = !camMouse;
-      cam.getMouseInputEnabled() ? cam.disableMouseInput() : cam.enableMouseInput();
+      camMouse ? cam.enableMouseInput() : cam.disableMouseInput();
 			break;
       
+    // saves the camera and mesh settings
     case 's':
       ofxSaveCamera(cam, "cameraSettings");
       mesh.save("mesh-tweaked.ply");
       break;
 
+    // loads camera and mesh settings
     case 'l':
       ofxLoadCamera(cam, "cameraSettings");
       mesh.load("mesh-tweaked.ply");
@@ -260,6 +246,7 @@ void ofApp::keyPressed(int key){
       editMode = false;
       break;
    
+    // show help
     case 'h':
       showHelp = !showHelp;
       break;
@@ -268,9 +255,7 @@ void ofApp::keyPressed(int key){
     case OF_KEY_TAB:
       editMode = !editMode;
       break;
-      
 	}
-  
 }
 
 //--------------------------------------------------------------
